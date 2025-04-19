a93f400105354e61cb215a74dc185cd7dbcbc040
yasindu12 <yasindusamarasinghe10@gmail.com>
2025-04-20 01:23:00 +0530

    handle some errors

--- middlewares/errorHandeller.js
+++ middlewares/errorHandeller.js
@@ -3,11 +3,11 @@ const CustomErrorHandeller = require('../errors/customError')
 const errorHandeller = (err, req, res, next) => {
     if (err instanceof CustomErrorHandeller) {
         console.log(`custom error ${err.message}`)
-        return res.status(err.stateCode).json({ success: false, msg: err.message })
+        return res.status(err.stateCode).json({ success: false, data: err.message })
     }
 
     console.log(`system error ${err.message}`)
-    res.status(500).json({ success: false, msg: err.message })
+    res.status(500).json({ success: false, data: err.message })
 }
 
 module.exports = errorHandeller
